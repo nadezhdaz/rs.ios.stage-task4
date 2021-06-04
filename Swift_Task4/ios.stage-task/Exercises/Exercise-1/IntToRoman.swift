5ca6e632c5626dd7c20d70d6cd5fd5244ffc8e60
nadyaios <nzenckova.ios@gmail.com>
2021-06-04 13:30:46 +0300

    feat: implemented exercises 1-2

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -1,8 +1,22 @@
 import Foundation
 
 public extension Int {
-    
     var roman: String? {
-        nil
+        let romanDictionary: KeyValuePairs = [1000 : "M", 900: "CM", 500 : "D", 400: "CD", 100 : "C", 90 : "XC", 50 : "L", 40 : "XL", 10 : "X", 9 : "IX", 5 : "V", 4 : "IV", 1 : "I"]
+        
+        var input = self
+        
+        if input >= 1 && input <= 3999 {
+            var result = ""
+            for (number, roman) in romanDictionary {
+                while input >= number {
+                    input -= number
+                    result += roman
+                }
+            }
+            return result
+        } else {
+            return nil
+        }
     }
 }
--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -3,6 +3,24 @@ import Foundation
 final class FillWithColor {
     
     func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
+        if image.count < 1, row > 50, image[row][column] < 0, newColor >= 65536 {
+            return image
+        }
+        
+        var pixelsToBeColored = image
+        let initialPixelValue = image[row][column]
+        pixelsToBeColored[row][column] = newColor
+        let nearestPixels = [(row + 1, column), (row - 1, column), (row, column + 1), (row, column - 1)]
+        
+        for (row, column) in nearestPixels {
+            if row >= 0, row < image.count, column >= 0, column < image[row].count {
+                if image[row][column] == initialPixelValue {
+                    pixelsToBeColored = fillWithColor(pixelsToBeColored, row, column, newColor)
+                }
+            }
+            
+        }
+        
+        return pixelsToBeColored
     }
 }
