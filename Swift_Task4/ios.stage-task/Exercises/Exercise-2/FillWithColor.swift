685901a0c8cfa06fc8e75c0d1c5e5932932f4607
nadyaios <nzenckova.ios@gmail.com>
2021-06-04 18:52:03 +0300

    fix: exercise 2 added constraints

--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -3,17 +3,22 @@ import Foundation
 final class FillWithColor {
     
     func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        if image.count < 1, row > 50, image[row][column] < 0, newColor >= 65536 {
+        guard let columns = image.first else {
+            return image
+        }
+        
+        if image.count < 1 || row < 0 || row >= image.count || row > 50 || column < 0 || columns.count < 1 || column >= columns.count || newColor >= 65536 || image[row][column] < 0 {
             return image
         }
         
         var pixelsToBeColored = image
         let initialPixelValue = image[row][column]
+        
         pixelsToBeColored[row][column] = newColor
         let nearestPixels = [(row + 1, column), (row - 1, column), (row, column + 1), (row, column - 1)]
         
         for (row, column) in nearestPixels {
-            if row >= 0, row < image.count, column >= 0, column < image[row].count {
+            if row >= 0 && row < image.count && column >= 0 && column < image[row].count {
                 if image[row][column] == initialPixelValue {
                     pixelsToBeColored = fillWithColor(pixelsToBeColored, row, column, newColor)
                 }
